0671435fe39e474f8ac0007a616382b1ace0544f
thienebst <thien.pham@ebst.tech>
2023-10-05 11:10:39 +0700

    add cart add recruitement

--- src/modules/recruitment/create/index.tsx
+++ src/modules/recruitment/create/index.tsx
@@ -1,7 +1,41 @@
-import React from 'react';
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  Container,
+  Divider,
+  Grid,
+  Typography
+} from '@mui/material';
+import Footer from 'src/components/Footer';
 
-const RecruitmentCreate = () => {
-  return <div>RecruitmentCreate</div>;
-};
+function RecruitmentCreate() {
+  return (
+    <>
+      <Container maxWidth="lg">
+        <Grid
+          container
+          direction="row"
+          justifyContent="center"
+          alignItems="stretch"
+          spacing={3}
+          marginTop={3}
+        >
+          <Grid item xs={12}>
+            <Card>
+              <CardHeader title="Tạo tin tuyển dụng" />
+              <Divider />
+              <CardContent>
+                <Typography variant="subtitle1">Thông tin cơ bản</Typography>
+                content
+              </CardContent>
+            </Card>
+          </Grid>
+        </Grid>
+      </Container>
+      <Footer />
+    </>
+  );
+}
 
 export default RecruitmentCreate;
