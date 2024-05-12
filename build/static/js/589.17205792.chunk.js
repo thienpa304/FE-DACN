32a99360b0344e2a275a4eb9a181744a51740ba3
viluong6457 <viluong6457@gmail.com>
2024-05-12 12:50:59 +0700

    run build

--- build/static/js/589.17205792.chunk.js
+++ build/static/js/589.17205792.chunk.js
@@ -1,14 +1,2 @@
-'use strict';
-(self.webpackChunkFE_DACN = self.webpackChunkFE_DACN || []).push([
-  [589],
-  {
-    27589: (s, e, u) => {
-      u.r(e), u.d(e, { default: () => k });
-      u(65043);
-      var a = u(89230),
-        c = u(70579);
-      const k = () => (0, c.jsx)(a.A, {});
-    }
-  }
-]);
+"use strict";(self.webpackChunkFE_DACN=self.webpackChunkFE_DACN||[]).push([[589],{27589:(s,e,u)=>{u.r(e),u.d(e,{default:()=>k});u(65043);var a=u(89230),c=u(70579);const k=()=>(0,c.jsx)(a.A,{})}}]);
 //# sourceMappingURL=589.17205792.chunk.js.map
