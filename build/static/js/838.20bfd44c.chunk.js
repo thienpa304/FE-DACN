b12ca21539a62e33f4b68273a714d4b55d393a63
viluong6457 <viluong6457@gmail.com>
2024-05-31 20:22:45 +0700

    update: feature search user, check employee is applied, fix: not allow edit job when application > 0

--- build/static/js/838.20bfd44c.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkFE_DACN=self.webpackChunkFE_DACN||[]).push([[838],{28838:(t,e,s)=>{s.r(e),s.d(e,{default:()=>u});var n=s(86971),d=s(86388),l=s(70579);const u=()=>{const{state:t}=(0,n.zy)(),e=t;return(0,l.jsx)(l.Fragment,{children:(0,l.jsx)(d.A,{title:"C\u1eadp Nh\xe2t tin tuy\u1ec3n d\u1ee5ng",selectedId:null===e||void 0===e?void 0:e.postId})})}}}]);
-//# sourceMappingURL=838.20bfd44c.chunk.js.map
