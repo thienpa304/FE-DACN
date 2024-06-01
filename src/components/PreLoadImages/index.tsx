bd8de16879da46aa0c2c8f245a446ec8bb49aeef
viluong6457 <viluong6457@gmail.com>
2024-06-01 19:05:02 +0700

    preload image

--- src/components/PreLoadImages/index.tsx
+++ src/components/PreLoadImages/index.tsx
@@ -1,27 +1,6 @@
-import { useEffect } from 'react';
 import { Helmet } from 'react-helmet-async';
-import { casouselImages } from 'src/pages/home/Casousel';
-
-// const professionImg = professions.map((item) => item.icon);
-const casouselImg = casouselImages.map((item) => item.lg);
-const logo = '/static/images/logo/web-logo.png';
-
-const imagesToPreload = [
-  // Add more image paths as needed
-  ...casouselImg,
-  logo
-];
 
 const PreloadImages = () => {
-  useEffect(() => {
-    imagesToPreload.forEach((src) => {
-      const link = document.createElement('link');
-      link.rel = 'preload';
-      link.href = src;
-      link.as = 'image';
-      document.head.appendChild(link);
-    });
-  }, [imagesToPreload]);
   return (
     <Helmet>
       <link rel="preconnect" href="https://www.flaticon.com" />
