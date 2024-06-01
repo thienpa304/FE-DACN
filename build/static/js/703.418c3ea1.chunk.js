544335aa990ad1085008bd88b019f8ac564cfa47
viluong6457 <viluong6457@gmail.com>
2024-06-02 01:30:52 +0700

    Remove build folder from dev branch

--- build/static/js/703.418c3ea1.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkFE_DACN=self.webpackChunkFE_DACN||[]).push([[703,793],{61259:(e,n,u)=>{u.r(n),u.d(n,{default:()=>c});var s=u(54567),t=u(64188),a=u(70579);const c=function(){return(0,a.jsx)(t.A,{pageTitle:"C\xf4ng vi\u1ec7c \u0111ang tuy\u1ec3n",queryJobs:s.A})}}}]);
-//# sourceMappingURL=703.418c3ea1.chunk.js.map
